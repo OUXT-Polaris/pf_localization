07fbd66b2b964f0ae45a9cba6c4c01af85ad0a8e
MasayaKataoka <ms.kataoka@gmail.com>
2019-05-04 00:12:42 +0900

    add PfLocalization class

--- include/pf_localization/pf_localization.h
+++ include/pf_localization/pf_localization.h
@@ -1,9 +1,17 @@
 #ifndef PF_LOCALIZATION_PF_LOCALIZATION_H_INCLUDED
 #define PF_LOCALIZATION_PF_LOCALIZATION_H_INCLUDED
 
+// Headers in this package
+#include <pf_localization/particle_filter.h>
+
 // Headers in ROS
 #include <ros/ros.h>
 #include <geometry_msgs/TwistStamped.h>
+#include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+
+// Headers in STL
+#include <memory>
 
 class PfLocalization
 {
@@ -12,6 +20,8 @@ public:
     ~PfLocalization();
 private:
     void twistStampedCallback(const geometry_msgs::TwistStamped::ConstPtr msg);
+    void pointStampedCallback(const geometry_msgs::PointStamped::ConstPtr msg);
+    std::shared_ptr<ParticleFilter> pf_ptr_;
 };
 
 #endif  //PF_LOCALIZATION_PF_LOCALIZATION_H_INCLUDED
--- src/pf_localization.cpp
+++ src/pf_localization.cpp
@@ -7,10 +7,15 @@ PfLocalization::PfLocalization(ros::NodeHandle nh,ros::NodeHandle pnh)
 
 PfLocalization::~PfLocalization()
 {
-    
+
 }
 
 void PfLocalization::twistStampedCallback(const geometry_msgs::TwistStamped::ConstPtr msg)
 {
+    return;
+}
 
+void PfLocalization::pointStampedCallback(const geometry_msgs::PointStamped::ConstPtr msg)
+{
+    return;
 }
