fa8aceadfd53b9e3de03d61fd7b9f3a329bdff80
Masaya Kataoka <ms.kataoka@gmail.com>
2019-06-04 08:11:33 +0900

    add interpolate virtual function

--- include/pf_localization/data_buffer_base.h
+++ include/pf_localization/data_buffer_base.h
@@ -15,13 +15,49 @@ class DataBufferBase
 public:
     DataBufferBase(std::string key,double buffer_length) : key(key),buffer_length(buffer_length){}
     ~DataBufferBase(){}
-    void queryData(ros::Time from,ros::Time to,std::string key,std::vector<T>& pose)
+    void queryData(ros::Time from,ros::Time to,std::string key,std::vector<T>& ret)
+    {
+        mtx_.lock();
+        ret.clear();
+        if(data_.size() == 0)
+        {
+            mtx_.unlock();
+            return;
+        }
+        for(auto itr = data_.begin(); itr != data_.end(); itr++)
+        {
+            if(itr->header.stamp > from && itr->header.stamp < to)
+            {
+                ret.push_back(*itr);
+            }
+        }
+        mtx_.unlock();
+        return;
+    }
+    void update()
+    {
+        std::vector<T> data;
+        mtx_.lock();
+        ros::Time now = ros::Time::now();
+        ros::Time target_timestamp = now - ros::Duration(buffer_length);
+        for(auto itr = data_.begin(); itr != data_.end(); itr++)
+        {
+            if(itr->header.stamp > target_timestamp)
+            {
+                data.push_back(*itr);
+            }
+        }
+        data_ = data;
+        mtx_.unlock();
+    }
+    void reorderData()
     {
         mtx_.lock();
         std::sort(data_.begin(), data_.end(), std::bind(&DataBufferBase::compareTwistTimeStamp, this,std::placeholders::_1, std::placeholders::_2));
         mtx_.unlock();
     }
-    virtual bool queryData(ros::Time timestamp, std::string key,T& pose){};
+    virtual bool queryData(ros::Time timestamp, std::string key,T& ret) = 0;
+    virtual T interpolate(T data0,T data1,ros::Time stamp) = 0;
     const std::string key;
     const double buffer_length;
 private:
--- include/pf_localization/pose_data_buffer.h
+++ include/pf_localization/pose_data_buffer.h
@@ -1,8 +1,12 @@
 #ifndef PF_LOCALIZATION_POSE_DATA_BUFFER_H_INCLUDED
 #define PF_LOCALIZATION_POSE_DATA_BUFFER_H_INCLUDED
 
+//headers in this package
 #include <pf_localization/data_buffer_base.h>
+
+//headers in ROS
 #include <geometry_msgs/PoseStamped.h>
+#include <quaternion_operation/quaternion_operation.h>
 
 class PoseDataBuffer : public DataBufferBase<geometry_msgs::PoseStamped>
 {
@@ -10,6 +14,8 @@ public:
     PoseDataBuffer(std::string key,double buffer_length);
     ~PoseDataBuffer();
     bool queryData(ros::Time timestamp, std::string key,geometry_msgs::PoseStamped& pose) override;
+private:
+    geometry_msgs::PoseStamped interpolate(geometry_msgs::PoseStamped data0,geometry_msgs::PoseStamped data1,ros::Time stamp) override;
 };
 
 
